bf36d340b6af631fc11be125b33bace0219f6509
Eduard Cespedes <eduard.cespedes@dsquare.be>
2016-11-07 19:15:19 +0100

    Adding CouchDB Integration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var nano = require('nano')('http://localhost:5984');
+var users = nano.db.use('users');
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    users.list(function (err, body) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            var usersDB = body.rows;
+            console.log(usersDB);
+            res.send(usersDB);
+        }
+    });
 });
 
 module.exports = router;
+
+
+
+
